41586605fae07bad424800bfd06ba1a1e2b59722
21JakubP37 <146883552+21JakubP37@users.noreply.github.com>
2024-03-14 08:54:32 +0100

    Add files via upload

---
+++ php_01_widok_kontroler/app/calc_view.php
@@ -0,0 +1,40 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator</title>
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<label for="id_wys">Wysokość Kredytu: </label>
+	<input id="id_wys" type="text" name="wys" value="<?php print($wys); ?>" /><br />
+	<label for="id_lata">Oprocentowanie Kredytu: </label>
+	<input id="id_lata" type="text" name="lata" value="<?php print($lata); ?>" /><br />
+	<label for="id_op">Ilość Lat: </label>
+	<input id="id_op" type="text" name="op" value="<?php print($op); ?>" /><br />
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Wynik: '.$result; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
